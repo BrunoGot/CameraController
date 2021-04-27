e3c83ab81cd07ed6c16956f80c5563577628053b
DESKTOP-E60HUU4\Natspir <Bruno.got@orange.fr>
2021-04-27 11:53:40 +0200

    Clean the code of the client (controller side)

--- client.py
+++ client.py
@@ -4,45 +4,6 @@ import network
 import socket
 import time
 
-#########Houdini connection##################"
-"""
-import time
-import socket
-from struct import pack
-"""
-
-"""class HoudiniSender():
-
-    def __init__(self):
-        self.esc = chr(170)
-        self.nul = chr(0)
-
-    def sendReset(self, conn):
-        conn.send(pack('cccccccc', self.esc, self.nul, self.esc, self.nul, self.esc, self.nul, self.esc, self.nul))
-
-    def send(self, conn, value):
-        for ch in value:
-            if ch == self.esc:
-                conn.send(ch)
-            conn.send(ch)
-
-    def sendCurrentValues(self, conn, channelCount, samples):
-        commandType = 1
-
-        # send command type
-        conn.send(pack('>Q', commandType))
-
-        # send channelCount
-        conn.send(pack('>Q', channelCount))  # packed as unsigned long long integer > 8bytes
-
-        # send samples
-        for s in samples:
-            print("send datas")
-            self.send(conn, pack('>d', s))  # packed as double float > 8bytes
-"""
-
-############################"
-
 class Kernel():
 
     def __init__(self):
@@ -76,14 +37,6 @@ class Kernel():
         wlan.connect(ssid, mdp)
         while (wlan.isconnected() == False):
             time.sleep(0.1)
-        """    def activate_server(self, ip, port):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind((ip, port))
-        s.listen(1)
-        conn, addr = s.accept()  # conn is the socket to communicate with the client
-        print('connection established')  # this is where i'm stuck
-        return conn"""
 
     def init_socket(self):
         """define the socket of the client"""
@@ -101,17 +54,6 @@ class Kernel():
         # set all the PWR_MGMT_1 to 0 to make sure the device is not in sleep mode
         i2c.writeto_mem(0x68, 0x6B, bytes([0]))
         while (True):
-            """#read the temperature from the register. These value are on two bytes:
-            temp_h = i2c.readfrom_mem(0x68,0x41,1)
-            temp_l = i2c.readfrom_mem(0x68,0x42,1)
-            #combine the high and lox bytes
-            temp_h = temp_h[0]
-            temp_l = temp_l[0]
-            #shifting hight byte to 8 places to the left
-            temp_h = temp_h<<8
-            #combine the high and low bytes
-            temp_data = temp_h | temp_l"""
-            temp_data = self.read_two_byte(i2c, 0x68, 0x41, 0x42, 1)
             lsbg = 16384.0  # lsb sensitvity number defined from the range table of the accelerometer
             accel_x = self.read_two_byte(i2c, 0x68, 0x3b, 0x3c, 1) / lsbg
             accel_y = self.read_two_byte(i2c, 0x68, 0x3d, 0x3e, 1) / lsbg
@@ -122,29 +64,11 @@ class Kernel():
             gyro_y = self.read_two_byte(i2c, 0x68, 0x45, 0x46, 1) / lsbds
             gyro_z = self.read_two_byte(i2c, 0x68, 0x47, 0x48, 1) / lsbds
 
-            # check if negative value, then adjust to positive val
-            if temp_data & 0b1000000000000000:
-                temp_data = -((temp_data ^ 0b1111111111111111) + 1)
-            # convert it to celcius value
-            temp_c = (temp_data / 340.0) + 36.53
-            # print("temp ="+str(temp_c))
-
-            print('datas = "accel = " + str(round(accel_x, 1)) + " | " + str(round(accel_y, 1)) + " | " + str('
-                  'round(accel_z, 1)) + " | gyro = " + str(round(gyro_x, 1)) + " | " + str(round(gyro_y, 1)) + " | " + str('
-                  'round(gyro_z, 1))')
-
             datas = str(accel_x)
-
-            ###########################################
 
             ##########WIFI###########
             # connect to access point
             self.connect_to_access_point(self.ssid, self.mdp)
-            """wlan = network.WLAN(network.STA_IF)
-            wlan.active(True)
-            wlan.connect("TP-LINK_B02DB9", "bmadekxcts8gmn2zbzlaayei6wf")
-            while(wlan.isconnected()==False):
-                time.sleep(0.1)"""
 
             #########################
 
@@ -152,17 +76,8 @@ class Kernel():
             # open socket and send datas :
             self.socket = self.init_socket()
             self.send_datas(datas)
-            """ip = "192.168.1.38"  # wlan.ifconfig()[0]
-            print(ip)
-            msg = datas
-            port = 10086            
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # udp
-            sock.sendto(msg, (ip, port))"""
-            #conn = self.activate_server(ip, port)
             #########################
 
-            # print(chr(27)+"[2J")
-            # print("accel z = "+str(accel_z))
 
 
 if __name__ == "__main__":
